081e1561e9ee6a0e9861779b9ecbc3baff15a4d4
Brian Anthony Jacobe <brian.a.jacobe@gmail.com>
2022-02-28 21:42:30 -0800

    Included a data-structures/algorithims repo for practice

---
+++ data-structures.js
@@ -0,0 +1,17 @@
+/**
+ * Practice module for data structures in JavaScript
+ * 
+ * Notes:
+ *  - Arrays
+ *  - Stack
+ *  - Queue
+ *  - Linked list
+ *  - Trees
+ *  - Hashtables
+ */
+
+//Arrays
+let myArray = ["first name","last name", "address"];
+myArray.forEach(item => {
+    console.log(item);
+});
