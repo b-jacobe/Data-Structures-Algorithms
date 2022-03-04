03a24fa838f216cc7b61c60fe878088e8ff3d8e2
Brian Anthony Jacobe <brian.a.jacobe@gmail.com>
2022-03-03 23:00:31 -0800

    added linked-list data structure

---
+++ linked-list.js
@@ -0,0 +1,16 @@
+/**
+ * Practice module for linked list in JavaScript
+ * 
+ * [Linked List](https://github.com/trekhleb/javascript-algorithms/tree/master/src/data-structures/linked-list)
+ * A linked list is a linear collection of data elements
+ * in which linear order is not given by their physical
+ * placement in memory but instead have each element
+ * point to the next via a group of nodes.
+ *   - Pros:
+ *      • Efficient insertion or removal of elements
+ *        from any position in sequence during iteration.
+ *   - Cons:
+ *      • Access time is linear and difficult to pipeline.
+ *      • Not greate cache locality.
+ * 
+ */
