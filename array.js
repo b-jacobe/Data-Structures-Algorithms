36338ebd3c315980afaa690a63a4807c51839351
Brian Anthony Jacobe <brian.a.jacobe@gmail.com>
2022-03-03 22:02:57 -0800

    updated array.js to include boilerplate header and static methods

--- array.js
+++ array.js
@@ -1,9 +1,24 @@
 /**
  * Practice module for arrays in JavaScript
+ * 
+ * [Arrays](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array)
+ * The array object stores a collection of items under a 
+ * single variable name. NOTE: Arrays are not primitives
+ * but contain the following object characteristics:
+ *   - Resizable and contain a mix of different types
+ *   - Not associative so therefore cannot be accessed
+ *     using strings but integers as index
+ *   - Zero-indexed
  */
 
-//Arrays
+// ======== ARRAYS ========
+// Constructor
+Array();
+
+// Declaration and output
 let myArray = ["first name","last name", "address"];
 myArray.forEach(item => {
     console.log(item);
 });
+
+console.log(Array.isArray(myArray));
