9a956299634ca98753e93cf7d5fd949f8f358477
Brian Anthony Jacobe <brian.a.jacobe@gmail.com>
2023-12-03 13:10:04 -0800

    Adding reverse vowels interview problem. This problem requires the two pointer technique

---
+++ interviews/arrays/reverse-vowels.js
@@ -0,0 +1,28 @@
+/**
+ * Given a string s, reverse only all the 
+ * vowels in the string and return it.
+ * The vowels are 'a', 'e', 'i', 'o', and 'u', 
+ * and they can appear in both lower and upper cases, 
+ * more than once.
+ */
+
+
+class Solution {
+    reverseVowels(s) {
+      // TODO: Write your code here
+      const vowels = new Set(['a','e','i','o','u','A','E','I','O','U']);
+      let convertArray = Array.from(s);
+      let i = 0, j = convertArray.length-1;
+      while (i < j ){
+        if(!vowels.has(convertArray[i])){
+          i++;
+        } else if (!vowels.has(convertArray[j])){
+          j--;
+        } else {
+          [convertArray[i], convertArray[j]] = [convertArray[j], convertArray[i]];
+          i++;
+          j--;
+        }
+      }
+      return convertArray.join('');
+}
